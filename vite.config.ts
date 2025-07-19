1324ed3f6aabbcd8df9debefba180bc27f8307d2
neycha <regentmarysapro@gmail.com>
2025-07-19 14:43:29 +0200

    chore: config_client

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-    plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      logic: path.resolve(__dirname, '../logic-qcm-plus/src'),
+    },
+  },
 });
