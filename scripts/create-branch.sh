ed81c412035b3dd9be4d6f3051e281b14d1bf36b
neycha <regentmarysapro@gmail.com>
2025-07-28 16:42:46 +0200

    feat: première configuration du pipeline - Fixes #1

--- scripts/create-branch.sh
+++ scripts/create-branch.sh
@@ -7,7 +7,7 @@
 # Vérifie que l'on est bien dans un dépôt Git
 if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
   echo "Erreur : ce répertoire n'est pas un dépôt Git."
-  exit 1
+  exit 1 
 fi
 
 # Vérifie les arguments
