816ad24a4a963918bc54540149df9188a133199c
MathieuHeitz <mheitzmann1@hotmail.fr>
2025-09-22 15:34:13 +0200

    development of home page and sidebar, first commit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,70 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import CustomSidebar from '../components/CustomSidebar';
 
 const HomePage: React.FC = () => {
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold mb-4">Bienvenue sur QCM-PLUS</h1>
-      <Link
-        to="/users"
-        className="inline-block bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+    <div
+      className="homepage background-image"
+      style={{
+        display: 'flex',
+        height: '100vh', 
+        overflow: 'hidden', 
+        position: 'relative',
+      }}
+    >
+      {/* Image de fond */}
+      <img
+        src="src/assets/images/fond.png"
+        alt="Background"
+        className="background-image"
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          zIndex: 0,
+        }}
+      />
+
+      {/* Sidebar */}
+      <CustomSidebar role="Admin" />
+
+      {/* Contenu principal */}
+      <div
+        className="content"
+        style={{
+          alignContent: 'center',
+          position: 'fixed',
+          top: '50%',
+          left: '55%',
+          transform: 'translate(-50%, -50%)',
+          display: 'flex',
+          alignItems: 'center',
+          zIndex: 1,
+        }}
       >
-        Gérer les utilisateurs
-      </Link>
+        <h1
+          style={{
+            margin: 0,
+            color: '#C8C8C6',
+            fontFamily: 'Inter sans-serif',
+          }}
+        >
+          Bienvenue sur
+        </h1>
+        <img
+          src="src/assets/images/LogoQCM+SansFond.png"
+          alt="Logo"
+          className="logo"
+          style={{
+            marginLeft: '5px',
+            width: '20em',
+            height: 'auto',
+          }}
+        />
+      </div>
     </div>
   );
 };
