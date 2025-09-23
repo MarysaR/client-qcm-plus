98eb4a5076d1beda8f6c3dfdf72ae2fb40255759
MathieuHeitz <mheitzmann1@hotmail.fr>
2025-09-23 11:01:11 +0200

    fix lint

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import '../styles/style.css';
 
 const Footer: React.FC = () => {
-    return (
-        <footer className="footer">
-            <p>@ 2025 QCM+ Tous droits réservés.</p>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <p>@ 2025 QCM+ Tous droits réservés.</p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/ProfilPage.tsx
+++ src/pages/ProfilPage.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
 const Profil: React.FC = () => {
-
-
-    return (
-        <div style={{marginLeft:'50%'}}>
-            <h1>Page Profil</h1>
-            <p>Ceci est la page de profil.</p>
-        </div>
-    );
+  return (
+    <div style={{ marginLeft: '50%' }}>
+      <h1>Page Profil</h1>
+      <p>Ceci est la page de profil.</p>
+    </div>
+  );
 };
 
 export default Profil;
