f421ea723fc7bd637229a5af55a2495ad6206d1e
neycha <regentmarysapro@gmail.com>
2025-09-27 14:58:42 +0200

    chore: config client to communicate with logic via simplink

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
-      logic: path.resolve(__dirname, 'vendor/logic-qcm-plus/src'),
+      'logic-qcm-plus': path.resolve(__dirname, 'vendor/logic-qcm-plus/src'),
     },
   },
   server: {
