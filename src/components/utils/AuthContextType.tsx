fcf411b1f681ab8e73d55d24b1bea544ce778ed6
neycha <regentmarysapro@gmail.com>
2025-09-27 14:59:17 +0200

    feat: delete files frontend

--- src/components/utils/AuthContextType.tsx
+++
@@ -1,8 +0,0 @@
-import { TokenClaims } from 'logic-qcm-plus';
-
-export interface AuthContextType {
-  token: string | null;
-  claims: TokenClaims | null;
-  login: (token: string, claims: TokenClaims) => Promise<void>;
-  logout: () => void;
-}
