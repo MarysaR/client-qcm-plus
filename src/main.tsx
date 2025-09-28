74df18840de8cc67d1262438860f84f39a923ad9
MathieuHeitz <mheitzmann1@hotmail.fr>
2025-09-28 09:42:20 +0200

    merging with develop before MR

--- src/main.tsx
+++ src/main.tsx
@@ -21,7 +21,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AuthProvider>
       <BrowserRouter>
-        {/* TODO: Passer le role en props dynamiquement */}
         {/* Sidebar*/}
         <CustomSidebar />
 
--- src/pages/users/UsersPage.tsx
+++ src/pages/users/UsersPage.tsx
@@ -38,7 +38,7 @@ export default function UsersPage() {
   return (
     <div style={{ marginLeft: '50%' }}>
       <h1>Page Users</h1>
-      <p>Ceci est la page de statistiques.</p>
+      <p>Ceci est la page des users.</p>
     </div>
   );
 }
