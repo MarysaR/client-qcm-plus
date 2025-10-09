8eb6801a5530997d73172e632535430a1eb892cb
neycha <regentmarysapro@gmail.com>
2025-10-09 18:25:25 +0200

    feat: change path profil to me, delete comments and review sidebar margin and fix logout style

--- src/components/sidebar/CustomSidebar.tsx
+++ src/components/sidebar/CustomSidebar.tsx
@@ -36,7 +36,7 @@ const CustomSidebar: React.FC<{ role: string }> = ({ role }) => {
 
   const menuItems = role
     ? [
-        { icon: 'pi pi-user', label: 'Profil', path: '/profil' },
+        { icon: 'pi pi-user', label: 'Profil', path: '/me' },
         {
           icon: 'pi pi-question-circle',
           label: 'Questionnaires',
@@ -45,7 +45,7 @@ const CustomSidebar: React.FC<{ role: string }> = ({ role }) => {
         { icon: 'pi pi-chart-bar', label: 'Statistiques', path: '/statistics' },
       ]
     : [
-        { icon: 'pi pi-user', label: 'Profil', path: '/profil' },
+        { icon: 'pi pi-user', label: 'Profil', path: '/me' },
         { icon: 'pi pi-users', label: 'Stagiaires', path: '/users' },
         {
           icon: 'pi pi-question-circle',
@@ -85,7 +85,6 @@ const CustomSidebar: React.FC<{ role: string }> = ({ role }) => {
         dismissable={false}
         modal={false}
       >
-        {/* Header avec logo et bouton retour */}
         <div className="sidebar-header">
           <span className="logo">
             <img src="src/assets/images/LogoQCM+Premiuim.PNG" alt="Logo" />
@@ -111,7 +110,6 @@ const CustomSidebar: React.FC<{ role: string }> = ({ role }) => {
           ))}
         </div>
 
-        {/*Déconnexion */}
         <div className="sidebar-logout">
           <a className="p-ripple" onClick={handleLogout}>
             <i className="pi pi-sign-out"></i>
