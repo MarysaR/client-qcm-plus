a559669f507b3bcc750b404c1ed6aba96115a663
cyndazagrouba <zagrouba.cynda@gmail.com>
2025-10-18 15:32:00 +0200

    resolve merge questionnaireService

--- src/services/questionnaire/questionnaireService.ts
+++ src/services/questionnaire/questionnaireService.ts
@@ -4,6 +4,8 @@ import {
   PermissionDeniedError,
   TechnicalError,
   ValidationError,
+  Result,
+  AppError,
 } from 'logic-qcm-plus';
 import { httpRequest, httpClient } from '../../utils/httpClient';
 import { mapHttpResult } from '../../utils/httpResultMapper';
@@ -101,6 +103,67 @@ export const questionnaireService = {
         return Err.of(mapHttpError(status));
       }
       return Ok.of(undefined);
+    });
+  },
+  async getQuestionnaireById(
+    id: number
+  ): Promise<Result<{ name: string; description?: string }, AppError>> {
+    const token = authService.getToken();
+    if (!token) {
+      return Err.of(
+        new PermissionDeniedError(
+          'Utilisateur non authentifié (token manquant)'
+        )
+      );
+    }
+
+    const resResult = await httpRequest(
+      httpClient.get(`/questionnaire/${id}`, {
+        headers: { Authorization: 'Bearer ' + token },
+      })
+    );
+
+    return mapHttpResult(resResult, async (res) => {
+      let status;
+      if ('status' in res) {
+        status = res.status;
+      } else {
+        status = (res as Response).status;
+      }
+
+      let body = {};
+      if ('data' in res) {
+        body = res.data;
+      } else {
+        const json = await (res as Response).json();
+        if (typeof json == 'object' && json !== null) {
+          body = json;
+        }
+      }
+
+      const statusInvalide =
+        status < HTTP_STATUS.OK ||
+        (status >= HTTP_STATUS.BAD_REQUEST &&
+          status < HTTP_STATUS.INTERNAL_SERVER_ERROR);
+
+      if (statusInvalide) {
+        return Err.of(mapHttpError(status));
+      }
+
+      const questionnaire = body as { name?: string; description?: string };
+
+      if (!questionnaire.name) {
+        return Err.of(
+          new TechnicalError(
+            'Réponse invalide du serveur (questionnaire incomplet)'
+          )
+        );
+      }
+
+      return Ok.of({
+        name: questionnaire.name,
+        description: questionnaire.description,
+      });
     });
   },
 };
